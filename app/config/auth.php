e44036dddd0df3ec5b6c385fb6efc22ba1eed051
Aldo Anizio Lugão Camacho <aldoanizio@yahoo.com.br>
2013-10-26 15:35:40 -0200

    Added table primary key in section config

--- app/config/auth.php
+++ app/config/auth.php
@@ -13,6 +13,7 @@ return array
      * username  : Table username field used to query registry from DB. Eg: username, email, login, nickname
      * password  : Table password field used to validate username/password combination
      * table     : Table where user is stored; Eg: users, administrators, members
+     * pk        : Table primary key field used to store session and cookie information; Eg: id
      * url       : Login URL Route. Used to redirect user to login screen and avoid redirect looping
      * redirect  : Define where to redirect user after Post Login. Set False if wish not redirect
      * ttl       : Cookie Time to live - if omitted or set to 0 the cookie will expire when the browser closes
@@ -20,12 +21,13 @@ return array
     
     'sections' => array
     (
-        'admin' => array
+        'users' => array
         (
             'secret'   => 'tx6T7nE7ZV5UinFSUngqFW04FlzhReBe',
             'login'    => 'email',
             'password' => 'password',
             'table'    => 'users',
+            'pk'       => 'id',
             'url'      => 'users/login',
             'redirect' => 'users',
             'ttl'      => 3600 * 720, // Eg: 30 Days
